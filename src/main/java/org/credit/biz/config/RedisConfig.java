a65377269ec85d785fda67a650b59c690bb2cf76
jackhu <jackhu.521.rose@gmail.com>
2026-04-22 10:49:09 +0800

    repo: 新增 Redis 缓存数据库 ---
    Details：新增数据库 Redis 连接，并测试了连接功能

---
+++ src/main/java/org/credit/biz/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package org.credit.biz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // 1. Key 的序列化采用 StringRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // 2. Value 的序列化采用 GenericJackson2JsonRedisSerializer (转为 JSON 格式)
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
